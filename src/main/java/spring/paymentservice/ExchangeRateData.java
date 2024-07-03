ed30ea12fa60ae42f03fbebaebce4b300b204682
jinhyeokko <jinhyeok.ko@outlook.com>
2024-07-03 10:35:56 +0900

    abstract inheritance

--- src/main/java/spring/paymentservice/ExchangeRateData.java
+++ src/main/java/spring/paymentservice/ExchangeRateData.java
@@ -1,2 +1,10 @@
-package spring.paymentservice;public record ExchangeRateData() {
+package spring.paymentservice;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record ExchangeRateData(String result, Map<String, BigDecimal> rates) {
 }
--- src/main/java/spring/paymentservice/PaymentService.java
+++ src/main/java/spring/paymentservice/PaymentService.java
@@ -1,2 +1,20 @@
-package spring.paymentservice;public class PaymentService {
+package spring.paymentservice;
+
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Service
+abstract public class PaymentService {
+    public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
+        BigDecimal exRate = getExRate(currency);
+        BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
+        LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
+
+        return new Payment(orderId, currency, foreignCurrencyAmount, exRate, convertedAmount, validUntil);
+    }
+
+    abstract BigDecimal getExRate(String currency) throws IOException;
 }
